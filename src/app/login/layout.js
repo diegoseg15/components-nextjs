3b02c1d8065c10d8d0302f9812f58b52de6b3233
diegoseg_15 <diegofersv@gmail.com>
2024-02-20 14:10:46 -0500

    :construction: Work in progress on login form

---
+++ src/app/login/layout.js
@@ -0,0 +1,3 @@
+export default function layout({ children }) {
+    return <div className="flex justify-center">{children}</div>;
+  }
---
+++ src/app/login/page.js
@@ -0,0 +1,11 @@
+export default function LoginPage(){
+    return(
+        <div>
+            <form>
+                <input type="email" placeholder="email"/>
+                <input type="password" placeholder="password"/>
+                <button>login</button>
+            </form>
+        </div>
+    )
+}
